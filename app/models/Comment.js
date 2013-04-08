9f9bf38b14f708ec919b126caaaed4f522a510f6
Alberto Blázquez Rodríguez <hola@albertoblazquez.net>
2013-04-08 16:47:59 +0200

    Add the Entry.

--- app/models/Comment.js
+++ app/models/Comment.js
@@ -5,3 +5,19 @@ var Comment = function(user, text) {
 
     this.date = new Date();
 };
+
+
+Comment.prototype.update = function(user, text, date) {
+    this.user   = user;
+    this.text = text;
+    this.date    = date;
+
+    for (var o in this.observers) {
+        o.notify();
+    }
+};
+
+
+Comment.prototype.addObservers = function(obs) {
+    this.observers.push(obs);
+};
